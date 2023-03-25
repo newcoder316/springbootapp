98e32a33c802d094d899067c76db72b0658c5ab2
taku <derenzhuo@gmail.com>
2023-03-25 11:26:27 +0900

    1

---
+++ src/main/java/com/springboot/taku/takuapp/TakuappApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.taku.takuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakuappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakuappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/taku/takuapp/TakuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.taku.takuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakuappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
